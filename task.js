873a0870f4cea936ed7813642e0dca00086222d1
Danil Krutikov <kdanil.i@yandex.ru>
2023-12-25 20:20:36 +0700

    Second commit

--- task.js
+++ task.js
@@ -1,12 +1,28 @@
+const formElement = document.getElementById("form");
 formElement.addEventListener("submit", (e) => {
-  const formElement = document.getElementById("form");
   const formData = new FormData(formElement);
   const file = formData.get("file");
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload")
-  xhr.send(file)
-  xhr.upload.onprogress = function(e) {
-    const progress = document.getElementById("progress");
-    progress.value += e.loaded
+  if (file) {
+    upload(file)
   }
 })
+
+function upload(file) {
+  let xhr = new XMLHttpRequest();
+  // обработчик для отправки
+  xhr.upload.onprogress = function(event) {
+    const progress = document.getElementById("progress");
+    progress.value += event.total / e.loaded;
+  }
+  // обработчики успеха и ошибки
+  // если status == 200, то это успех, иначе ошибка
+  xhr.onload = xhr.onerror = function() {
+    if (this.status == 200) {
+      log("success");
+    } else {
+      log("error " + this.status);
+    }
+  };
+  xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload")
+  xhr.send(file)
+}
